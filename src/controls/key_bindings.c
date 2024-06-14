c2f1928093d79a60c46b72c74955ec52cef76cb0
parallels <parallels@debian-gnu-linux-12.localdomain>
2024-06-14 16:57:52 +0200

    sace

--- src/controls/key_bindings.c
+++ src/controls/key_bindings.c
@@ -81,42 +81,8 @@ int	loop_hook(t_game *game)
 		rotate_player_left(game);
 	if (game->keys[RIGHT_ARROW])
 		rotate_player_right(game);
-	if (game->keys[E] && !game->door_are_closing && !game->door_are_opening)
-		open_close_door(game);
 	if (game->keys[ESC])
 		close_game(game);
-	if (game->door_are_opening)
-	{
-		struct timeval  tv;
-		gettimeofday(&tv, NULL);
-		long    current_time = tv.tv_sec * 1000000 + tv.tv_usec;
-		long    elapsed_time = current_time - game->door_animation_start_time;
-		int door_frame = elapsed_time / (DOOR_FRAME_DURATION / DOOR_FRAMES);
-		if (door_frame < 0)
-			door_frame = 0;
-		if (door_frame >= DOOR_FRAMES)
-		{
-			game->door_are_opening = false;
-			door_frame = DOOR_FRAMES - 1;
-		}
-		game->door_current_texture = game->door_texture[door_frame];
-	}
-	if (game->door_are_closing)
-	{
-		struct timeval  tv;
-		gettimeofday(&tv, NULL);
-		long    current_time = tv.tv_sec * 1000000 + tv.tv_usec;
-		long    elapsed_time = current_time - game->door_animation_start_time;
-		int door_frame = DOOR_FRAMES - 1 - elapsed_time / (DOOR_FRAME_DURATION / DOOR_FRAMES);
-		if (door_frame >= DOOR_FRAMES)
-			door_frame = DOOR_FRAMES - 1;
-		if (door_frame <= 0)
-		{
-			game->door_are_closing = false;
-			door_frame = 0;
-		}
-		game->door_current_texture = game->door_texture[door_frame];
-	}
 	render_map(game);
 	return (0);
 }
